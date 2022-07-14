badbf13e1cce166e141b8902a1b638ba1c94f3f6
Zeus Vargas <zeusvargas26@gmail.com>
2022-07-14 18:00:54 -0500

    fix port in backend

--- index.js
+++ index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js')
 const { conn } = require('./src/db.js')
 const { getTemperamentsAndSendThemToDb } = require('./src/controllers/dbFunctions')
+const { PORT } = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-	server.listen(5000, () => {
-		console.log('%s listening at 5000') // eslint-disable-line no-console
+	server.listen(PORT, () => {
+		console.log(`%s listening at ${PORT}`) // eslint-disable-line no-console
 	})
 })
 
